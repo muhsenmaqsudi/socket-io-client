aa35601ec1a2f2d52495273d1436266279edd4e0
muhsenmaqsudi <muhsenmaqsudi@gmail.com>
2020-08-26 14:41:09 +0430

    added tailwindcss & a telegram ui clone

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,15 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+  <Telegram />
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import Vue from "vue";
+import Telegram from "@/components/Telegram.vue"; // @ is an alias to /src
 
 export default Vue.extend({
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld,
+    Telegram,
   },
 });
 </script>
